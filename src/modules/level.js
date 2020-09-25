e480b0aa06cecd798b0473cbb496d0e1d7235922
Holyhoppsan <daniel.hall@live.se>
2020-09-25 15:48:05 +0100

    Added horizontal cell lines

--- src/modules/level.js
+++ src/modules/level.js
@@ -25,12 +25,19 @@ class Level {
     }
 
     render = (renderBuffer) => {
-        const cellWidth = 25;
+        const cellSize = 25;
 
         for (let x = 0; x < this.gridSize.x; x++) {
-            const xPosition = x * cellWidth;
+            const xPosition = x * cellSize;
             if (xPosition < renderBuffer.width) {
                 drawLineDDA(new Vector2D(xPosition, 0), new Vector2D(xPosition, renderBuffer.height), new Color(0, 255, 0, 255), renderBuffer);
+            }
+        }
+
+        for (let y = 0; y < this.gridSize.y; y++) {
+            const yPosition = y * cellSize;
+            if (yPosition < renderBuffer.height) {
+                drawLineDDA(new Vector2D(0, yPosition), new Vector2D(renderBuffer.width, yPosition), new Color(0, 255, 0, 255), renderBuffer);
             }
         }
     }
